8a302182cc0a305107811518d4e38a87074736f8
Aidan McGauley <40058902@ads.qub.ac.uk>
2023-08-03 15:30:28 +0100

    adding working maxmin

--- src/functions.inc.php
+++ src/functions.inc.php
@@ -1,5 +1,5 @@
 <?php
-function getMaxMin($items, $attendances)
+function getMaxMinOld($items, $attendances)
 {
     $item_attendances = array();
     for ($i = 0; $i < count($items); $i++) {
@@ -16,3 +16,24 @@ function getMaxMin($items, $attendances)
 
     return array($max_item,$min_item);
 }
+
+function getMaxMin($items, $attendances)
+{
+    $max_attendance = max($attendances);
+    $min_attendance = min($attendances);
+    $max_items = array();
+    $min_items = array();
+
+    for ($i = 0; $i < count($items); $i++) {
+        if ($attendances[$i] == $max_attendance) {
+            $max_items[] = $items[$i] . ' - ' . $attendances[$i];
+        }
+        if ($attendances[$i] == $min_attendance) {
+            $min_items[] = $items[$i] . ' - ' . $attendances[$i];
+        }
+    }
+
+    return array($max_items, $min_items);
+}
+
+?>
