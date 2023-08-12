a244a980faab1c6295a840dc74e765e4ac2404ce
Aidan McGauley <40058902@ads.qub.ac.uk>
2023-08-12 19:55:05 +0100

    Fixing yml file

--- src/functions.inc.php
+++ src/functions.inc.php
@@ -2,6 +2,11 @@
 
 function getMaxMin($items, $attendances)
 {
+    // Check if the input arrays are empty
+    if (empty($items) || empty($attendances)) {
+        return array([], []);
+    }
+    
     $max_attendance = max($attendances);
     $min_attendance = min($attendances);
     $max_items = array();
